d9714c42d733746d24041c8acdc8d8ddeaebb563
haoerdege <1048639298@qq.com>
2017-07-27 10:42:21 +0800

    Add files via upload

---
+++ NSDate+JWComparingTime.h
@@ -0,0 +1,27 @@
+//
+//  NSDate+JWComparingTime.h
+//  JWComparingTime
+//
+//  Created by JW on 2017/7/27.
+//  Copyright © 2017年 JW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (JWComparingTime)
+
+//比较from和self的时间差值
+-(NSDateComponents *)deltaFrom:(NSDate *)from;
+
+//是否为今年
+-(BOOL)isThisYear;
+
+//是否为今天
+-(BOOL)isToday;
+
+//是否为昨天
+-(BOOL)isYesterday;
+
+
+
+@end
